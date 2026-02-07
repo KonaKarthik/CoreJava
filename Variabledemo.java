c720a8aa76ad562c34c415b1662b0c1914578d2e
karthikeya <konakathik22@gmail.com>
2026-02-07 17:44:08 +0530

    variables

---
+++ Variabledemo.java
@@ -0,0 +1,11 @@
+public class Variabledemo{
+    public static void main(String[] args){
+        int price = 1000;  
+        double rating = 3.5;
+        String brand = "Nike";
+        System.out.println(price);
+        System.out.println("The price of the shirt is: "+price);
+        System.out.println("The rating of the shirt is: "+rating);
+        System.out.println("The brand of the shirt is: "+brand);
+    }
+}
