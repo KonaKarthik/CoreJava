fe08b9dac49a035b532c88cbb9cff4d563037246
karthikeya <konakathik22@gmail.com>
2026-02-07 17:44:20 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes{
+    public static void main(String[] args){
+        byte byteval = 10;
+        short shortval = 10000; 
+        int intval = 462632363;
+        float floatval = 5.3f;
+        double doubleval = 54.85464;
+        char charval = 'c';
+        boolean bollval = true;
+        String name = "Karthikeya";
+        System.out.println(byteval);
+        System.out.println(shortval);
+        System.out.println(intval);
+        System.out.println(floatval);
+        System.out.println(doubleval);
+        System.out.println(charval);
+        System.out.println(bollval);
+        System.out.println(name);
+    }
+}
